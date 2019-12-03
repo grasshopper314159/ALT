f1db3414cfbe4ab1a81ca8d78b20a5286d78bc88
Jordan <jordan.theisen302@gmail.com>
2019-12-02 22:59:14 -0700

    Account Settings Touch-Ups
    The buttons in account settings now work properly.

---
+++ static/PAGENAME/JS/PAGENAME.js
@@ -0,0 +1,25 @@
+function displayAccount()
+{
+  document.getElementById("changeableText").innerHTML = "Account Settings";
+}
+
+function displayPrivacy()
+{
+  document.getElementById("changeableText").innerHTML = "Privacy Settings";
+}
+
+function displayLanguage()
+{
+  document.getElementById("changeableText").innerHTML = "Language Settings";
+}
+
+function displayAccessibility()
+{
+  document.getElementById("changeableText").innerHTML = "Accessibility Settings";
+}
+
+function displayTerms()
+{
+  document.getElementById("changeableText").innerHTML = "Terms and Conditions";
+}
+
