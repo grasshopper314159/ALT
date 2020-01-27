9da89274d3be7a59d404a47a74887fdf2d341993
Jordan <jordan.theisen302@gmail.com>
2020-01-26 19:37:51 -0700

    Database Tables - First Wave
    All entities and attributes.

--- alr_site/alr/models.py
+++ alr_site/alr/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    num_audio_files = models.IntegerField()
+    last_login_date = models.DateTimeField()
+    user_type = models.CharField(max_length = 15)
+    password = models.CharField(max_length = 20)
+    firstname = models.CharField(max_length = 20)
+    lastname = models.CharField(max_length = 20)
+    email = models.EmailField()
+    id = models.IntegerField(primary_key = True)
+    
+class AudioTrim(models.Model): #Weak Entity
+    original_text = models.TextField()
+    english_text = models.TextField()
+    phonetic_text = models.TextField()
+    last_listened_date = models.DateTimeField()
+    measurements = models.IntegerField() #Not sure what the data type for this should be
+    score = models.IntegerField()
+    word_count = models.IntegerField()
+    length = models.IntegerField()
+    start_time = models.TimeField() #Dashed underline
+    
+#Comment and Review are attributes to the relationship between BigAudio and User
+class BigAudio(models.Model):
+    id = models.IntegerField(primary_key = True)
+    upload_date = models.DateField()
+    length = models.TimeField()
+    
+class Class(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField()
+    id = models.IntegerField(primary_key = True)
+    
+class Speaker(models.Model):
+    id = models.IntegerField(primary_key = True)
+    firstname = models.CharField(max_length = 20)
+    lastname = models.CharField(max_length = 20)
+
+class Language(models.Model):
+    id = models.IntegerField(primary_key = True)
+    name = models.CharField(max_length = 50)
