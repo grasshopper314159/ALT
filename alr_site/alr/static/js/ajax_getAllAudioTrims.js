6235d949e658f3ce3e85d7d801f6bd2cc1d7369c
Unknown <bosscodycollege@yahoo.com>
2020-02-07 14:08:15 -0700

    viewdata

--- alr_site/alr/static/js/ajax_getAllAudioTrims.js
+++ alr_site/alr/static/js/ajax_getAllAudioTrims.js
@@ -7,22 +7,30 @@ $(document).ready(function(){
 
       success: function(response)
       {
-        Options = document.getElementById('options');
-        select = Options.innerHTML;
+        data = document.getElementById('data');
+        owner = 'Me';
+        speaker = 'you';
+        project = 'mine';
+        text_original = 'Hello';
+        text_english = 'Hello';
+        score = '7';
+        date = '02/07/2020';
+        console.log('HELP');
         for (var key in response) {
-          optgroup = '<optgroup label=\"' + key + '\">';
-          for (var i = 0; i < response[key].length; i++) {
-            value = response[key][i];
-            option = '<option value=\"' + (key+','+value) + '\">' + value + '</option>';
-            optgroup += option;
-          }
-          optgroup += '</optgroup>';
-
-          console.log(optgroup);
-          select += optgroup;
-        }
-        Options.innerHTML = select;
-
+            entry = '<tr><td>' +
+              '<div class="w3-bar">' +
+              '<button class="w3-button w3-tiny w3-white w3-border w3-round">Edit</button>'+
+              '<button class="w3-button w3-tiny w3-white w3-border w3-round">Del</button>'+
+              '</div></td>' +
+              '<td>' + owner + '</td>' +
+              '<td>' + speaker + '</td>' +
+              '<td>' + project + '</td>' +
+              '<td>' + text_original + '</td>' +
+              '<td>' + text_english + '</td>' +
+              '<td>' + score + '</td>' +
+              '<td>' + date + '</td>' +
+              '<td><button class="w3-btn w3-white w3-block w3-border w3-round">Show</button>' +
+              '</td></tr>';
       },
 
       failure: function()
@@ -44,30 +52,6 @@ $(document).ready(function(){
 //       success: function(response)
 //       {
 //         alert('success')
-//         data = document.getElementById('data');
-//         owner = 'Me';
-//         speaker = 'you';
-//         project = 'mine';
-//         text_original = 'Hello';
-//         text_english = 'Hello';
-//         score = '7';
-//         date = '02/07/2020';
-//         console.log('HELP');
-//         for (var key in response) {
-//             entry = '<tr><td>' +
-//               '<div class="w3-bar">' +
-//               '<button class="w3-button w3-tiny w3-white w3-border w3-round">Edit</button>'+
-//               '<button class="w3-button w3-tiny w3-white w3-border w3-round">Del</button>'+
-//               '</div></td>' +
-//               '<td>' + owner + '</td>' +
-//               '<td>' + speaker + '</td>' +
-//               '<td>' + project + '</td>' +
-//               '<td>' + text_original + '</td>' +
-//               '<td>' + text_english + '</td>' +
-//               '<td>' + score + '</td>' +
-//               '<td>' + date + '</td>' +
-//               '<td><button class="w3-btn w3-white w3-block w3-border w3-round">Show</button>' +
-//               '</td></tr>';
 //             data.innerHTML += entry;
 //       },
 //
