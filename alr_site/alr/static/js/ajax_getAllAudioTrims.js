9b11f59fed9603836cc5897077e8d1652c4ca569
Unknown <bosscodycollege@yahoo.com>
2020-02-07 14:10:48 -0700

    fix

--- alr_site/alr/static/js/ajax_getAllAudioTrims.js
+++ alr_site/alr/static/js/ajax_getAllAudioTrims.js
@@ -17,20 +17,21 @@ $(document).ready(function(){
         date = '02/07/2020';
         console.log('HELP');
         for (var key in response) {
-            entry = '<tr><td>' +
-              '<div class="w3-bar">' +
-              '<button class="w3-button w3-tiny w3-white w3-border w3-round">Edit</button>'+
-              '<button class="w3-button w3-tiny w3-white w3-border w3-round">Del</button>'+
-              '</div></td>' +
-              '<td>' + owner + '</td>' +
-              '<td>' + speaker + '</td>' +
-              '<td>' + project + '</td>' +
-              '<td>' + text_original + '</td>' +
-              '<td>' + text_english + '</td>' +
-              '<td>' + score + '</td>' +
-              '<td>' + date + '</td>' +
-              '<td><button class="w3-btn w3-white w3-block w3-border w3-round">Show</button>' +
-              '</td></tr>';
+          entry = '<tr><td>' +
+            '<div class="w3-bar">' +
+            '<button class="w3-button w3-tiny w3-white w3-border w3-round">Edit</button>'+
+            '<button class="w3-button w3-tiny w3-white w3-border w3-round">Del</button>'+
+            '</div></td>' +
+            '<td>' + owner + '</td>' +
+            '<td>' + speaker + '</td>' +
+            '<td>' + project + '</td>' +
+            '<td>' + text_original + '</td>' +
+            '<td>' + text_english + '</td>' +
+            '<td>' + score + '</td>' +
+            '<td>' + date + '</td>' +
+            '<td><button class="w3-btn w3-white w3-block w3-border w3-round">Show</button>' +
+            '</td></tr>';
+        }
       },
 
       failure: function()
