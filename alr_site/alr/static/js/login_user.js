fdc997d11472a416174ec273b8e8dae9757af01e
Unknown <bosscodycollege@yahoo.com>
2020-02-07 13:55:06 -0700

    update

--- alr_site/alr/static/js/login_user.js
+++ alr_site/alr/static/js/login_user.js
@@ -1,19 +1,1 @@
-// $(document).ready(function(){
-//   $.ajax(
-//     {
-//       type: 'POST',
-//       url: '/ajax/login_user/',
-//
-//       success: function(response)
-//       {
-//         alert("AJAX Succeded!");
-//       },
-//
-//       failure: function()
-//       {
-//         alert("AJAX FAILED!");
-//       }
-//     }
-//   );
-// });
 $("login_user").ajaxSubmit({url: '/ajax/loginUser', type: 'POST'})
