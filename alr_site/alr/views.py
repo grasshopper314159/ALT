721202455af59d5f1ad1987707e0404def4858a2
Hill-Boss <bosscodycollege@yahoo.com>
2020-03-04 22:17:35 -0700

    little fixes

--- alr_site/alr/views.py
+++ alr_site/alr/views.py
@@ -295,48 +295,48 @@ def ajax_logoutUser(request):
 
 # this is not even close to being done
 @csrf_exempt
+@login_required(login_url='/home/')
 def ajax_postUploadAudio(request):
-    fileOwner = alr_user.objects.get(id=User.objects.get(email=str(request.user)))
+    if is_user_type(request, ['ADMIN','research_user'], OR=True):
+        if request.method == 'POST':
+            fileOwner = alr_user.objects.get(id=User.objects.get(email=str(request.user)))
 
-    
-    fileFile = request.POST['fileToUpload']
-    #fileLength = request.POST['fileLength']
-    fileLength = "00:01:03"
-    fileSpeakerFirst = request.POST['fileSpeakerFirst']
-    fileSpeakerLast = request.POST['fileSpeakerLast']
-    fileSpeakerId = None
-    # TODO: Add langauge if new language given
-    fileLanguageId = Language.objects.filter(name=request.POST['fileLanguageId'])[0]
 
-    try:
-        # if speaker in db already
-        fileSpeakerId = Speaker.objects.filter(first_name=fileSpeakerFirst, last_name=fileSpeakerLast)[0]
-    except Exception as e:
-        pass
-        print("Failed to get speaker")
-        print(e)
-    else:
-        # add speaker to DB
-        try:
-            pass
-        # if new speaker is already a user
-            #newSpeaker = Speaker(first_name=fileSpeakerFirst, last_name=fileSpeakerLast, user_id=alr_user.objects.filter(id=User.objects.filter(first_name=fileSpeakerFirst, last_name=fileSpeakerLast)[0].id)[0])
-            #newSpeaker.save()
-            #fileSpeakerId = newSpeaker.id
-        except Exception as e:
-            raise
-        else:
-            pass
-            # newSpeaker = Speaker(first_name=fileSpeakerFirst, last_name=fileSpeakerLast, user_id=None)
-            # newSpeaker.save()
-            # fileSpeakerId = newSpeaker[0].id
-    big_audio = BigAudio(sound_file=fileFile, length = fileLength, owner_id=fileOwner, speaker_id=fileSpeakerId, language_id=fileLanguageId)
-    big_audio.save()
+            fileFile = request.POST['fileToUpload']
+            #fileLength = request.POST['fileLength']
+            fileLength = "00:01:03"
+            fileSpeakerFirst = request.POST['fileSpeakerFirst']
+            fileSpeakerLast = request.POST['fileSpeakerLast']
+            fileSpeakerId = None
+            # TODO: Add langauge if new language given
+            fileLanguageId = Language.objects.get(name=request.POST['fileLanguageId'])
 
-    #active_messages["audioTrim"] = str(big_audio.id) + ' ' + str('')
-    active_messages["audioTrim"] = str(1) + ' ' + str('artica_0003.wav')
+            try:
+                # if speaker in db already
+                fileSpeakerId = Speaker.objects.get(first_name=fileSpeakerFirst, last_name=fileSpeakerLast)
+            except Exception as e:
+                pass
+                print("Failed to get speaker")
+                print(e)
+            else:
+                # add speaker to DB
+                try:
+                    # if new speaker is already a user
+                    newSpeaker = Speaker(first_name=fileSpeakerFirst, last_name=fileSpeakerLast, user_id=alr_user.objects.get(id=User.objects.get(first_name=fileSpeakerFirst, last_name=fileSpeakerLast)))
+                    newSpeaker.save()
+                    fileSpeakerId = newSpeaker
+                except Exception as e:
+                    raise
+                else:
+                    newSpeaker = Speaker(first_name=fileSpeakerFirst, last_name=fileSpeakerLast, user_id=None)
+                    newSpeaker.save()
+                    fileSpeakerId = newSpeaker
+            big_audio = BigAudio(sound_file=fileFile, length = fileLength, owner_id=fileOwner, speaker_id=fileSpeakerId, language_id=fileLanguageId)
+            big_audio.save()
 
-    return redirect("/trimAudio/")
+            #active_messages["audioTrim"] = str(big_audio.id) + ' ' + str('fileFile.name')
+            active_messages["audioTrim"] = str(1) + ' ' + str('artica_0003.wav')
+            return redirect("/trimAudio/")
 
 #end of ajax calls
 
